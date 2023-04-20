b56c8d6bd662919b40869e341d9e1c9f7d0fbfd8
Minhaj Khan <minhajaswe@gmail.com>
2023-04-20 14:56:36 -0400

    movieland api running

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import { useEffect } from 'react';
+
+// a313473b note:click link to activate api key
+const API_URL = 'http://www.omdbapi.com?apikey=a313473b'
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+      const response = await fetch(`${API_URL}&s=${title}`)
+      const data = await response.json();
+      
+      console.log(data.Search)
+    }
+
+  useEffect(() => {
+    searchMovies('Spiderman');
+  },[]);
+
   return (
     <div className="App">
-        <h1> Hello, React! </h1>
+        <h1> App </h1>
     </div>
   );
 }
